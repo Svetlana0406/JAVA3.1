8ba9693800f522d2c8575a593242d32ca146dc39
Svetlana0406 <75991471+Svetlana0406@users.noreply.github.com>
2021-01-13 20:20:15 +0300

    Add files via upload

---
+++ .gitignore/JAVA3.1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 1021;
+        int bonus;
+        bonus = amount/20;
+        System.out.println(bonus);
+    }
+}
+
+
